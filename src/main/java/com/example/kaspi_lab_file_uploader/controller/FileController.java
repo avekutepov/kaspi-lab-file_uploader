ded5178b8e5b2f28e6431961c4a85e6c3cde0973
Pavel <kutepov_pasha@list.ru>
2026-02-05 18:43:50 +0500

    Update controller

--- src/main/java/com/example/kaspi_lab_file_uploader/controller/FileController.java
+++ src/main/java/com/example/kaspi_lab_file_uploader/controller/FileController.java
@@ -1,32 +1,23 @@
 package com.example.kaspi_lab_file_uploader.controller;
 
-import com.example.kaspi_lab_file_uploader.model.FileUploadDTO;
-import com.example.kaspi_lab_file_uploader.service.FileProcessingService;
+import com.example.kaspi_lab_file_uploader.service.FileUploadService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/files")
 @AllArgsConstructor
 public class FileController {
-    private final FileProcessingService service;
+    private final FileUploadService service;
 
     @PostMapping("/upload")
-    public void uploadFile(
-            @RequestParam("file")MultipartFile file
-            ) throws IOException {
-        FileUploadDTO dto = new FileUploadDTO(
-                file.getName(),
-                file.getSize(),
-                file.getContentType(),
-                file.getInputStream());
-
-
+    public ResponseEntity<Long> uploadFile(@RequestParam MultipartFile file){
+        return ResponseEntity.accepted().body(service.startUpload(file));
     }
 }
