dc4f5adf5c37abffe02d891e20a2549ef0e4354a
Pavel <kutepov_pasha@list.ru>
2026-02-05 18:38:41 +0500

    Add AsyncConfig

---
+++ src/main/java/com/example/kaspi_lab_file_uploader/config/AsyncConfig.java
@@ -0,0 +1,27 @@
+package com.example.kaspi_lab_file_uploader.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "fileUploadExecutor")
+    public Executor fileUploadExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(8);
+        executor.setQueueCapacity(50);
+
+        executor.setThreadNamePrefix("file-upload-");
+
+        executor.initialize();
+        return executor;
+    }
+}
