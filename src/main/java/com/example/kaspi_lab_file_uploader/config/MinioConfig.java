d2332009273f27ba0a89fa353c49853633e7f9c8
Pavel <kutepov_pasha@list.ru>
2026-02-05 18:39:15 +0500

    Add storage config

--- src/main/java/com/example/kaspi_lab_file_uploader/config/MinioConfig.java
+++ src/main/java/com/example/kaspi_lab_file_uploader/config/MinioConfig.java
@@ -3,7 +3,9 @@ package com.example.kaspi_lab_file_uploader.config;
 import io.minio.MinioClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class MinioConfig {
 
     @Value("${minio.url}")
@@ -16,7 +18,7 @@ public class MinioConfig {
     private String secretKey;
 
     @Bean
-    public MinioClient client() {
+    public MinioClient minioClient() {
         return MinioClient.builder()
                 .endpoint(baseUrl)
                 .credentials(accessKey, secretKey)
